f8e5da597160bea5a245617d3b6e2d3ef0c7bfca
jeekkaaaa <root@telegraf.home.jeekkaaaa>
2025-12-16 12:44:51 +0000

    FIX: Remove unsupported provider arguments

--- template/main.tf
+++ template/main.tf
@@ -11,10 +11,6 @@ provider "proxmox" {
   endpoint  = var.pm_api_url
   api_token = "${var.pm_api_token_id}=${var.pm_api_token_secret}"
   insecure  = true
-  
-  # Таймаут соединения для провайдера (в миллисекундах)
-  http_retry_max     = 5
-  http_retry_timeout = 30000  # 30 секунд
 }
 
 # Загрузка образа с увеличенными таймаутами
